1be8ea4a27670bd6fb47edf228e6352d7118b497
vega-d <35414314+vega-d@users.noreply.github.com>
2021-12-20 18:38:08 +0100

    init
    initial commit

---
+++ build/remix.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+# Add universe and multiverse.
+add-apt-repository -y --no-update universe
+add-apt-repository -y --no-update multiverse
+apt-get update -y
+
+# Install software
+apt-get purge -y snapd
+apt-mark hold snapd 
+apt-get install -y wget curl gnome-tweaks git flatpak gnome-software gnome-software-plugin-flatpak
+flatpak remote-add --system flathub https://flathub.org/repo/flathub.flatpakrepo
+
+# Installs gnome extensions.
+apt install gnome-shell-extension-gsconnect gnome-shell-extension-no-annoyance
+# bash ./install-gnome-extensions.sh --enable 517 779 448 4228
+# Extensions are as follows: caffeine clipboard-indicator remove-rounded-corners sound-output-device-chooser
