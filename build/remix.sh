17097981348a79cc591fe56e698e9e7fb9ea6cd4
ThePenguinUser <83493195+ThePenguinUser@users.noreply.github.com>
2021-12-22 15:46:37 +0100

    Replace apt-get with apt

--- build/remix.sh
+++ build/remix.sh
@@ -3,12 +3,12 @@
 # Add universe and multiverse.
 add-apt-repository -y --no-update universe
 add-apt-repository -y --no-update multiverse
-apt-get update -y
+apt update -y
 
 # Install software
-apt-get purge -y snapd
+apt purge -y snapd
 apt-mark hold snapd 
-apt-get install -y wget curl gnome-tweaks git flatpak gnome-software gnome-software-plugin-flatpak
+apt install -y wget curl gnome-tweaks git flatpak gnome-software gnome-software-plugin-flatpak
 flatpak remote-add --system flathub https://flathub.org/repo/flathub.flatpakrepo
 
 # Installs gnome extensions.
